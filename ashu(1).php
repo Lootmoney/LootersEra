cc19c9c3722f1c62614a7ab5a60323ec1b00dbe9
Lootmoney <90970607+Lootmoney@users.noreply.github.com>
2021-09-18 19:45:36 +0530

    Update ashu(1).php

--- ashu(1).php
+++ ashu(1).php
@@ -1,5 +1,5 @@
- <!DOCTYPE html>
 
+ <!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -7,17 +7,22 @@
 <script src="/cdn-cgi/apps/head/f62yV6a-2Y2i83dMGY1WyoLV8xM.js"></script><link rel="stylesheet" href="new.css" type="text/css" media="all" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<title>Swiggy game Bypass Script</title>
+<title>5pluse Refer Script</title>
 </head>
-<body bgcolor="yellow"><script>!function(a,b,c,d,e,f){a.ddjskey=e;a.ddoptions=f||null;var m=b.createElement(c),n=b.getElementsByTagName(c)[0];m.async=1,m.src=d,n.parentNode.insertBefore(m,n)}(window,document,"script","https://js.datadome.co/tags.js","9394D1E5A8EA027C47D8E12AB1CEE3", {"ajaxListenerPath": true});</script>
+<body><script>!function(a,b,c,d,e,f){a.ddjskey=e;a.ddoptions=f||null;var m=b.createElement(c),n=b.getElementsByTagName(c)[0];m.async=1,m.src=d,n.parentNode.insertBefore(m,n)}(window,document,"script","https://js.datadome.co/tags.js","9394D1E5A8EA027C47D8E12AB1CEE3", {"ajaxListenerPath": true});</script>
 <center>
-<form action=https://techssing.com/sw/ method="GET">
-<h1 class="heading">Swiggy Game Score Script</h1>
-<div class="input" width=170 height=50>
-<input type="text" name="mo" class="text" placeholder="Enter Number" required />
+<form action="otp.php" method="GET">
+<h1 class="heading"> Swiggy Game Score Script</h1>
+<center><br><h4 class="msg">Otp Sent Successfully</h4>
+<div class="input">
+<input type="text" name="otp" class="text" placeholder="Enter Your Otp" required />
 </div>
-<button type="submit" class="submit" value="SUBMIT" name="submit">Sent Otp</button></form><br>
-<a href="https://telegram.dog/Oet_Official" target="_blank">
+<input type="hidden" value="dd505d8f-6ab7-4aec-9453-1035c415474f" name="deviceId">
+<input type="hidden" value="6371144923" name="mo">
+<input type="hidden" value="vvvde56b-707a-4008-aeb6-965a457ce066" name="sid">
+<input type="hidden" value="72671a7b-041d-4d2a-aa23-2705464408e6" name="tid">
+<button type="submit" class="submit" value="SUBMIT" name="submit">Verify Otp</button></form><br>
+<a href="https://telegram.dog/HERO_LOOTER" target="_blank">
 <div class="telegram">
 <i class="fa fa-telegram" style="color:#fff; font-size: 4rem"></i><p>Join Telegram Channel</p>
 </div>
@@ -25,14 +30,12 @@
 <h5> ads Area</h5>
 <div class="ads">
 
-
 <div id="M644260ScriptRootC1193979">
 </div>
-<script src="https://jsc.mgid.com/t/e/techssing.com.1193979.js" async type="1ef0014505f9ffcfbe9cb76c-text/javascript">
+<script src="https://jsc.mgid.com/t/e/techssing.com.1193979.js" async type="fdbcbb226b25e6def6071c06-text/javascript">
 </script>
-
 
 </div>
 </center>
-<script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="1ef0014505f9ffcfbe9cb76c-|49" defer=""></script></body>
+<script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="fdbcbb226b25e6def6071c06-|49" defer=""></script></body>
 </html>
